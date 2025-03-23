d3c31447a4a353789816ba9020f42cede4acdffa
Felipe Justiniano <felipejustiniano19@outlook.com>
2025-03-23 19:19:08 -0300

    User insert

--- src/main/java/br/com/felipe/course/Controller/UserResources.java
+++ src/main/java/br/com/felipe/course/Controller/UserResources.java
@@ -3,11 +3,10 @@ package br.com.felipe.course.Controller;
 import br.com.felipe.course.entities.User;
 import br.com.felipe.course.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,5 +29,12 @@ public class UserResources {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
